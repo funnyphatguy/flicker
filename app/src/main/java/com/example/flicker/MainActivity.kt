d855051ce2d4b224304b4f149ffa2a649492f43d
alber <contius32@gmail.com>
2026-05-22 14:10:57 +0300

    feat: add compose dependencies, delete xml layout

--- app/src/main/java/com/example/flicker/MainActivity.kt
+++ app/src/main/java/com/example/flicker/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.example.flicker
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            Text(
+                text = "Flicker",
+                modifier = Modifier.statusBarsPadding()
+            )
         }
     }
 }
