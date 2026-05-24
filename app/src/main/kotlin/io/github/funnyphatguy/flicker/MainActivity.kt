f1f299a508da2ecf06230261189b0213ebb728a6
alber <contius32@gmail.com>
2026-05-24 13:44:11 +0300

    refactor: change package to io.github.funnyphatguy.flicker and clean up directories

--- app/src/main/kotlin/io/github/funnyphatguy/flicker/MainActivity.kt
+++ app/src/main/kotlin/io/github/funnyphatguy/flicker/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
